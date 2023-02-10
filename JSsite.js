e8c76e749b7326e64f321b7bb21702e53ca97ce3
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-02-10 01:26:29 -0300

    getting brutal material

---
+++ JSsite.js
@@ -0,0 +1,649 @@
+//window.onload = function () {
+//  document.getElementById('myDIV').style.display = 'none';
+//};
+
+window.onload = function () {
+  document.getElementById('myDIV').style.display = 'none';
+  document.getElementById('myDIV2').style.display = 'none';
+  document.getElementById('myDIV3').style.display = 'none';
+  document.getElementById('myDIV4').style.display = 'none';
+  document.getElementById('myDIV5').style.display = 'none';
+  document.getElementById('myDIV6').style.display = 'none';
+  document.getElementById('myDIV7').style.display = 'none';
+  document.getElementById('myDIV8').style.display = 'none';
+  document.getElementById('myDIV9').style.display = 'none';
+  document.getElementById('myDIV10').style.display = 'none';
+  document.getElementById('myDIV11').style.display = 'none';
+  document.getElementById('myDIV12').style.display = 'none';
+  document.getElementById('myDIV13').style.display = 'none';
+  document.getElementById('myDIV14').style.display = 'none';
+  document.getElementById('myDIV15').style.display = 'none';
+  document.getElementById('myDIV16').style.display = 'none';
+  document.getElementById('myDIV17').style.display = 'none';
+  document.getElementById('myDIV18').style.display = 'none';
+  document.getElementById('myDIV19').style.display = 'none';
+  document.getElementById('myDIV20').style.display = 'none';
+  document.getElementById('myDIV21').style.display = 'none';
+  document.getElementById('myDIV22').style.display = 'none';
+  document.getElementById('myDIV23').style.display = 'none';
+  document.getElementById('myDIV24').style.display = 'none';
+  document.getElementById('myDIV25').style.display = 'none';
+  document.getElementById('myDIV26').style.display = 'none';
+  document.getElementById('myDIV27').style.display = 'none';
+  document.getElementById('myDIV28').style.display = 'none';
+  document.getElementById('myDIV29').style.display = 'none';
+};
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("myDIV2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction3() {
+    var x = document.getElementById("myDIV3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction4() {
+    var x = document.getElementById("myDIV4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction5() {
+    var x = document.getElementById("myDIV5");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction6() {
+    var x = document.getElementById("myDIV6");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction7() {
+    var x = document.getElementById("myDIV7");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction8() {
+    var x = document.getElementById("myDIV8");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction9() {
+    var x = document.getElementById("myDIV9");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction10() {
+    var x = document.getElementById("myDIV10");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction11() {
+    var x = document.getElementById("myDIV11");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction12() {
+    var x = document.getElementById("myDIV12");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction13() {
+    var x = document.getElementById("myDIV13");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction14() {
+    var x = document.getElementById("myDIV14");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction15() {
+    var x = document.getElementById("myDIV15");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction16() {
+    var x = document.getElementById("myDIV16");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction17() {
+    var x = document.getElementById("myDIV17");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction18() {
+    var x = document.getElementById("myDIV18");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction19() {
+    var x = document.getElementById("myDIV19");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction20() {
+    var x = document.getElementById("myDIV20");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction21() {
+    var x = document.getElementById("myDIV21");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction22() {
+    var x = document.getElementById("myDIV22");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction23() {
+    var x = document.getElementById("myDIV23");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction24() {
+    var x = document.getElementById("myDIV24");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction25() {
+    var x = document.getElementById("myDIV25");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction26() {
+    var x = document.getElementById("myDIV26");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction27() {
+    var x = document.getElementById("myDIV27");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction28() {
+    var x = document.getElementById("myDIV28");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction29() {
+    var x = document.getElementById("myDIV29");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  
+function T1() {
+    var a = Number.parseInt(document.getElementById('a').value);
+    var b = Number.parseInt(document.getElementById('b').value);
+    var resp = 'O MDC é: ' + MDC(a,b);
+    document.getElementById('T1').innerHTML = resp;
+}
+
+function MDC(a,b){
+    if (b == 0) {
+        return a}
+    else {
+        return MDC(b, a % b)
+    } 
+
+}
+
+function T2() {
+    let r = Number.parseFloat(document.getElementById('r-1').value);
+    let i = Number.parseFloat(document.getElementById('i-1').value);
+    document.getElementById('T2').innerHTML = Tensão(r,i);
+    
+    function Tensão (r,i)  {
+    return ( (r * i) + ' ' + 'V');
+    }
+}
+
+
+
+function T3() {
+    var a = Number.parseFloat(document.getElementById('x2').value);
+    var b = Number.parseFloat(document.getElementById('x1').value);
+    var c = Number.parseFloat(document.getElementById('lin').value);
+    var resp = EQ(a,b,c,);
+    document.getElementById('T3').innerHTML = resp;
+}
+
+function EQ(a,b,c,) {
+    var delta = ((Math.pow(b,2)) - (4 * a * c));
+    var x1 = (((-1 * b) + (Math.sqrt(delta))) / (2 * a));
+    var x2 = (((-1 * b) - (Math.sqrt(delta))) / (2 * a));
+    if (delta<0) {
+        return ("As raízes da equação não pertencem ao conjunto dos números reais")
+    }
+    else {
+        if(x1 = x2){
+            return ("x' = x'' = " + x1);
+            }
+        else{
+        return("X' = " + x1 + 'e X" = ' + x2);
+        }
+
+    }
+}
+
+function T4() {
+    var V = Number.parseFloat(document.getElementById('vol').value);
+    var N = Number.parseFloat(document.getElementById('mol').value);
+    var T = Number.parseFloat(document.getElementById('temp').value);
+    var resp = EQG(V,N,T,);
+    document.getElementById('T4').innerHTML = resp;
+}
+
+function EQG(V,N,T,){
+    return ('Pressão: ' + ((N*(T+273)*0,082)/V + ' atm'));
+}
+
+function T5() {
+    var m = Number.parseFloat(document.getElementById('m').value);
+    var c = Number.parseFloat(document.getElementById('ce').value);
+    var t = Number.parseFloat(document.getElementById('dt').value);
+    var resp = QMCT(m,c,t);
+    document.getElementById('T5').innerHTML = resp;
+}
+
+function QMCT(m,c,t,){
+    return ('Quantidade de Calor: ' + (m*c*t) + ' cal');
+}
+
+function T6() {
+    var k = Number.parseFloat(document.getElementById('k').value);
+    var x = Number.parseFloat(document.getElementById('def').value);
+    var resp = EPEL(k,x,);
+    document.getElementById('T6').innerHTML = resp;
+}
+
+function EPEL(k,x,){
+    return ('Energia Potencial Elástica: ' + ((k*(Math.pow(x,2)))/2) + ' J');
+}
+
+function T7() {
+    var l = Number.parseFloat(document.getElementById('lambda').value);
+    var velp = Number.parseFloat(document.getElementById('velp').value);
+    var resp = F(l,velp,);
+    document.getElementById('T7').innerHTML = resp;
+}
+
+function F(l,velp,){
+    return ('Frequência de Onda: ' + (velp/l) + ' Hz');
+}
+
+function T8() {
+    var comp = Number.parseFloat(document.getElementById('l2').value);
+    var freq = Number.parseFloat(document.getElementById('hz').value);
+    var resp = VP(comp,freq);
+    document.getElementById('T8').innerHTML = resp;
+}
+
+function VP(comp,freq){
+    return ('Velocidade de Propagação: ' + (comp*freq) + ' m/s');
+}
+
+function T9() {
+    var vo = Number.parseFloat(document.getElementById('vo').value);
+    var a = Number.parseFloat(document.getElementById('ac').value);
+    var ds = Number.parseFloat(document.getElementById('ds').value);
+    var resp = TORR(vo,a,ds);
+    document.getElementById('T9').innerHTML = resp;
+}
+
+function TORR(vo,a,ds,){
+    return ('Velocidade Final: ' + (Math.sqrt((Math.pow(vo,2))+2*a*ds)) + ' m/s');
+}
+
+function T10() {
+    var n = Number.parseFloat(document.getElementById('mol2').value);
+    var v = Number.parseFloat(document.getElementById('litros').value);
+    var resp = C(n,v,);
+    document.getElementById('T10').innerHTML = resp;
+}
+
+function C(n,v,){
+    return ('Concentração: ' + (n/v) + ' mols/L');
+}
+
+function T11() {
+    var m = Number.parseFloat(document.getElementById('massa').value);
+    var v = Number.parseFloat(document.getElementById('litros2').value);
+    var resp = D(m,v,);
+    document.getElementById('T11').innerHTML = resp;
+}
+
+function D(m,v,){
+    return ('Densidade: ' + (m/v) + ' g/L');
+}
+
+function T12() {
+    var a1 = Number.parseFloat(document.getElementById('primeiro').value);
+    var an = Number.parseFloat(document.getElementById('enesimo').value);
+    var q = Number.parseFloat(document.getElementById('razao').value);
+    var resp = AN(a1,an,q);
+    document.getElementById('T12').innerHTML = resp;
+}
+
+function AN(a1,an,q){
+    return ('O '+ an + '° termo da sequência é: ' + (a1*(Math.pow(q,(an-1)))));
+}
+
+function T13() {
+    var a1 = Number.parseFloat(document.getElementById('pt').value);
+    var n = Number.parseFloat(document.getElementById('nt').value);
+    var q = Number.parseFloat(document.getElementById('razao2').value);
+    var resp = ST(a1,n,q);
+    document.getElementById('T13').innerHTML = resp;
+}
+
+function ST(a1,n,q){
+    return ('A soma dos termos é:  '+ ((a1 * ((Math.pow(q,n)-1)))/(q-1)));
+}
+
+function T14() {
+    var a1 = Number.parseFloat(document.getElementById('pa1').value);
+    var n = Number.parseFloat(document.getElementById('pa2').value);
+    var q = Number.parseFloat(document.getElementById('pa3').value);
+    var resp = PA1(a1,n,q);
+    document.getElementById('T14').innerHTML = resp;
+}
+
+function PA1(a1,n,q){
+    return ('O '+ n + '° termo da sequência é: ' + (a1+(q*(n-1))));
+}
+
+function T15() {
+    var a1 = Number.parseFloat(document.getElementById('pa4').value);
+    var an = Number.parseFloat(document.getElementById('pa5').value);
+    var n = Number.parseFloat(document.getElementById('pa6').value);
+    var resp = PA2(a1,an,n);
+    document.getElementById('T15').innerHTML = resp;
+}
+
+function PA2(a1,an,n){
+    return ('A soma dos termos é:  '+ (((a1+an)*n)/2));
+}
+
+function T16() {
+    var a1 = Number.parseFloat(document.getElementById('resf').value);
+    var resp = Math.PI*4/3*Math.pow(a1,3) + ' m³';
+    document.getElementById('T16').innerHTML = resp;
+}
+
+function T17() {
+    let u = Number.parseFloat(document.getElementById('u-1').value);
+    let i = Number.parseFloat(document.getElementById('i-2').value);
+    // Segue aqui seu código
+    document.getElementById('T17').innerHTML = Potencia(u,i);
+    
+    function Potencia (u,i)  {
+    return ((u * i) + ' ' + 'W');
+    }
+} 
+
+function T18() {
+    let m = Number.parseFloat(document.getElementById('mas').value);
+    let v = Number.parseFloat(document.getElementById('vms').value);
+    document.getElementById('T18').innerHTML = EnergiaCinética(m,v);
+     
+    function EnergiaCinética (m,v)  {
+    return ((m * (Math.pow(v,2))/2) + " " + "J");
+    }
+}
+
+function T19() {
+    let f = Number.parseFloat(document.getElementById('f-1').value);
+    let a = Number.parseFloat(document.getElementById('a-1').value);
+    
+    document.getElementById('T19').innerHTML = Pressão(f,a);
+    
+    function Pressão (f,a)  {
+    return ((f/a) + ' ' + "N/m²");
+    } 
+}
+
+function T20() {
+    let m = Number.parseFloat(document.getElementById('m-2').value);
+    let h = Number.parseFloat(document.getElementById('h-1').value);
+    
+    document.getElementById('T20').innerHTML = EnergiaPotencialGravitacional(m,h);
+    
+    function EnergiaPotencialGravitacional (m,h)  {
+    return ((10 * m * h) + ' ' + "J");
+    } 
+}
+
+function T21() {
+    let d = Number.parseFloat(document.getElementById('D-líquido').value);
+    let v = Number.parseFloat(document.getElementById('vss').value);
+    document.getElementById('T21').innerHTML = Empuxo(d,v);
+    
+    function Empuxo (d,v)  {
+    return ((d * v * 10) + ' ' + "N");
+    } 
+}
+
+function T22() {
+    let So = Number.parseFloat(document.getElementById('esp1').value);
+    let V = Number.parseFloat(document.getElementById('vo1').value);
+    let T = Number.parseFloat(document.getElementById('temp1').value);
+    let A = Number.parseFloat(document.getElementById('ac1').value);
+    let x = (Math.pow(T,2))/2
+    document.getElementById('T22').innerHTML = Sorvetão(So,V,T,A,x);
+    
+    function Sorvetão (So,V,T,A,x)  {
+    return ('Espaço Final: ' + (So + (V * T) + (A * x)) + ' ' + "m");
+    } 
+}
+
+function T23() {
+    let m1 = Number.parseFloat(document.getElementById('con1').value);
+    let m2= Number.parseFloat(document.getElementById('con2').value);
+    let v1= Number.parseFloat(document.getElementById('vl1').value);
+    let v2= Number.parseFloat(document.getElementById('vl2').value);
+    let vf= v1 + v2;
+    
+    document.getElementById('T23').innerHTML = ConcentraçãoFinalMistura(m1,m2,v1,v2,vf);
+    
+    function ConcentraçãoFinalMistura (m1,m2,v1,v2,vf)  {
+    return ((((m1 * v1) + (m2 * v2))/ vf) + ' ' + "Mol/L");
+    } 
+}
+
+function T24() {
+    let c1 = Number.parseFloat(document.getElementById('con3').value);
+    let v2= Number.parseFloat(document.getElementById('vl3').value);
+    let v1= Number.parseFloat(document.getElementById('vl4').value);
+   
+    
+    document.getElementById('T24').innerHTML = Concentração2(c1,v1,v2);
+    
+    function Concentração2 (c1,v1,v2)  {
+    return (((c1 * v1)/v2) + ' ' + "Mol/L");
+    } 
+}
+
+function T25() {
+    let c = Number.parseFloat(document.getElementById('cin').value);
+    let i = Number.parseFloat(document.getElementById('jur').value);
+    let t = Number.parseFloat(document.getElementById('tre').value);
+    let aux = i/100;
+   
+    
+    document.getElementById('T25').innerHTML = JurosCompostos(aux,t,c);
+    
+    function JurosCompostos (aux,t,c)  {
+    return ('R$ ' + (c * (Math.pow((1 + aux),t))).toFixed(2));
+    } 
+}
+
+function T26() {
+    let l = Number.parseFloat(document.getElementById('Lado').value);
+    
+    
+    document.getElementById('T26').innerHTML = AreaTriânguloEquilátero(l);
+    
+    function AreaTriânguloEquilátero (l)  {
+    return (((Math.pow(l,2)* Math.sqrt(3))/4) + ' ' + "m²");
+    } 
+}
+
+function T27() {
+    let l = Number.parseFloat(document.getElementById('ldif').value);
+    let h = Number.parseFloat(document.getElementById('alt').value);
+    document.getElementById('T27').innerHTML = AreaTriânguloIsóceles(l,h);
+    
+    function AreaTriânguloIsóceles (l,h)  {
+    return (((l * h)/2) + ' ' + "m²");
+    } 
+}
+
+function T28() {
+    let Maior = Number.parseFloat(document.getElementById('Base Maior').value);
+    let Menor = Number.parseFloat(document.getElementById('Base Menor').value);
+    let h = Number.parseFloat(document.getElementById('Altura do Trapézio').value);
+   
+    
+    document.getElementById('T28').innerHTML = AreaTrapézio(Maior,Menor,h);
+    
+    function AreaTrapézio (Maior,Menor,h)  {
+    return ((((Maior + Menor) * h)/2) + ' ' + "m²");
+    } 
+}
+
+function T29() {
+    let a = Number.parseFloat(document.getElementById('ladoa').value);
+    let b = Number.parseFloat(document.getElementById('ladob').value);
+    let c = Number.parseFloat(document.getElementById('ladoc').value);
+   
+    document.getElementById('T29').innerHTML = VolumeCubo(a,b,c);
+    
+    function VolumeCubo (a,b,c)  {
+    return ((a * b * c) + ' ' + "m³");
+    } 
+}
